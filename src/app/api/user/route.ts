1a66f6f3f86106ffcb6a8fdc813def4f7aed8c84
Nader Abdulrub <nabdulrub16@gmail.com>
2023-09-25 00:04:22 -0400

    Changed user route

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,15 +1,14 @@
 import { connectToDatabase } from "@/lib/connectdb";
 import { NextResponse } from "next/server";
 import { prisma } from "../../../../prisma";
-import bcrypt from "bcrypt";
-import { ZodError } from "zod";
 
 export const GET = async (req: Request, res: Response) => {
   try {
     await connectToDatabase();
-    const users = await prisma.user.findMany({});
+    const users = await prisma.user.findMany();
     return NextResponse.json({ users });
   } catch (error) {
     console.log(error);
+    return NextResponse.json({ message: "Internal Errorr" }, { status: 500 });
   }
 };
